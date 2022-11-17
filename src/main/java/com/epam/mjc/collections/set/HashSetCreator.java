b2ee1475be65205581acbc96706dcf1cb2ccbf4e
Saidahmad95 <murtazoyev00@gmail.com>
2022-11-17 23:51:15 +0500

    set_task first commit

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,18 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (Integer integer : sourceList) {
+            while (integer % 2 == 0) {
+                hashSet.add(integer);
+                integer = integer / 2;
+            }
+            if (integer%2!=0){
+                hashSet.add(integer);
+                hashSet.add(integer*2);
+            }
+
+        }
+        return hashSet;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,5 +5,19 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> firstSet1 = new HashSet<>(firstSet);
+        firstSet1.retainAll(secondSet);
+        firstSet1.removeAll(thirdSet);
+
+        Set<String> firstAndSecond = new HashSet<>(firstSet);
+        firstAndSecond.addAll(secondSet);
+
+        thirdSet.removeAll(firstAndSecond);
+
+        Set<String> result = new HashSet<>();
+        result.addAll(firstSet1);
+        result.addAll(thirdSet);
+
+        return result;
     }
 }
